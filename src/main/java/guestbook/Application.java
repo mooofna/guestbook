fcccbdead60e4ec2648871abdfd1ae34cc10896c
mooofna <105771470+mooofna@users.noreply.github.com>
2023-10-16 17:54:58 +0200

    - Entry field for email - Displaying email of card in GUI

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -60,12 +60,14 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
-					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
+					new GuestbookEntry("H4xx0r", "first!!!", "h4xxor@gmail.com"), //
+					new GuestbookEntry("Arni", "Hasta la vista, baby", "arni@gmail.com"), //
 					new GuestbookEntry("Duke Nukem",
-							"It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
+							"It's time to kick ass and chew bubble gum. And I'm all out of gum.",
+							"dukenukem@gmail.com"), //
 					new GuestbookEntry("Gump1337",
-							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
+							"Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+							"forrestgump@gmail.com")) //
 					.forEach(guestbook::save);
 		};
 	}
